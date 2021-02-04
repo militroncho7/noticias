40c5c71c0fefa6ce80616c980a71ddd14d91b910
militroncho7 <victor@MacBook-Air-de-Victor-2.local>
2021-02-04 10:25:35 +0100

    finish custom hooks

--- src/hooks/useSelect.js
+++ src/hooks/useSelect.js
@@ -3,14 +3,24 @@ import React, { useState } from 'react';
 const useSelect = (stateInicial, opciones) => {
 
     //state del csutom hook
-    const [ state, actualizarState ] = useState('');
-    //console.log(state);
+    const [ state, actualizarState ] = useState(stateInicial);
     
     const SelectNoticias = () => (
         <select
             className="browser-default"
+            value={state}
+            onChange={e => actualizarState(e.target.value)}
         >
-            <option value="">--Seleciona--</option>
+            <option key='general' value='general'>General</option>
+            <option key='business' value='business'>Negocios</option>
+            <option key='entertainment' value='entertainment'>Entretenimiento</option>
+            <option key='health' value='health'>Salud</option>
+            <option key='science' value='science'>Ciencia</option>
+            <option key='sports' value='sports'>Deportes</option>
+            <option key='technology' value='technology'>Tecnología</option>
+            {/* {opciones.map(opcion => (
+                <option key={opcion.value} value={opcion.value}>{opcion.label}</option>
+            ))}; */}
         </select>
     );
 
